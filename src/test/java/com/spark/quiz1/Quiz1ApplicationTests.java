1dfd1d42e92e6d81012a4d70e1d41e6bb23f561d
idoudist <fadi.idoudi@esprit.tn>
2019-02-11 17:27:17 +0100

    first commit

---
+++ src/test/java/com/spark/quiz1/Quiz1ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.spark.quiz1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Quiz1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
